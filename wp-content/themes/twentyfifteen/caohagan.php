d7ad18f8b5dc391cf21899e3d6762c739b0f72fc
mdsakibhasan <md.shakhaoathossain@gmail.com>
2015-12-22 17:52:31 +0600

    here is my day three work... making design fully ...

--- wp-content/themes/twentyfifteen/caohagan.php
+++ wp-content/themes/twentyfifteen/caohagan.php
@@ -14,8 +14,8 @@ Template Name: caohagan
 	<section>
 		<img class="oneimg" src="http://sakib.localhost/wp-content/themes/twentyfifteen/images/oneimg.png" alt="">
 		<img class="twoimg" src="http://sakib.localhost/wp-content/themes/twentyfifteen/images/twoimg.png" alt="">
-		<img class="threeimg">
-		<img class="fourimg">
+		<div class="threeimg"></div>
+		<div class="fourimg"></div>
 		<div class="yellow-box-1">
 			<div class="yellow-box-1-content">
 				<a href="#">here is the olivecode site 1</a>
@@ -31,14 +31,84 @@ Template Name: caohagan
 			</div>
 			<img src="http://sakib.localhost/wp-content/themes/twentyfifteen/images/arrow2.png" alt="yellow-box-2">
 		</div>
-		<img class="fiveimg">
-		<img class="siximg">
-		<img class="sevenimg">
-		<div class="fig-one-img-one">
+		<div class="fiveimg"></div>
+		<div class="siximg"></div>
+		<div class="sevenimg"></div>
+		<div class="fig-one-img">
+			<div class="fig-one-img-title-1">
+				<?php $post_id = 18; 
+				$queried_post = get_post($post_id); 
+				echo $queried_post->post_content;?>
+			</div>
+			<div class="fig-one-img-title-2">
+				<?php $post_id = 22; 
+				$queried_post = get_post($post_id); 
+				echo $queried_post->post_content;?>
+			</div>
+			<div class="fig-one-img-content-1">
+				<?php $post_id = 24; 
+				$queried_post = get_post($post_id); 
+				echo $queried_post->post_content;?>
+			</div>
+			<div class="fig-one-img-content-2">
+				<?php $post_id = 28; 
+				$queried_post = get_post($post_id); 
+				echo $queried_post->post_content;?>
+			</div>
 			<img class="fig-one-img-1" src="http://sakib.localhost/wp-content/themes/twentyfifteen/images/fig-one-img-1.png" alt="">
+			<div class="fig-one-img-content-3">
+				<?php $post_id = 30; 
+				$queried_post = get_post($post_id); 
+				echo $queried_post->post_content;?><br/><br/><hr>
+					<a href="#">カオハガン島の概要</a>
+					<a href="#" style="margin-left:50px;">島の沿革</a><br/>
+					<a href="#">カオハガン2050</a>
+					<a href="#" style="margin-left:50px;">NGO南の島から</a><br/>
+					<a href="#">熱帯珊瑚礁保護区</a>
+					<a href="#" style="margin-left:50px;">島主紹介</a>
+			</div>
+			<img class="fig01img" src="http://sakib.localhost/wp-content/themes/twentyfifteen/images/fig01img.png" alt="">
 			<img class="fig-one-img-2" src="http://sakib.localhost/wp-content/themes/twentyfifteen/images/fig-one-img-2.png" alt="">
 			<img class="fig-one-img-3" src="http://sakib.localhost/wp-content/themes/twentyfifteen/images/fig-one-img-3.png" alt="">
-		</div>>
+		</div>
+		<div class="fig-two-img">
+			<img class="fig-two-img-1" src="http://sakib.localhost/wp-content/themes/twentyfifteen/images/fig-two-img-1.png" alt="">
+			<img class="fig02img" src="http://sakib.localhost/wp-content/themes/twentyfifteen/images/fig02img.png" alt="">
+			<img class="fig-two-img-3" src="http://sakib.localhost/wp-content/themes/twentyfifteen/images/fig-two-img-3.png" alt="">
+		</div>
+		<div class="eightimg"></div>
+		<div class="fig-three-img">
+			<img class="fig-three-img-1" src="http://sakib.localhost/wp-content/themes/twentyfifteen/images/fig-three-img-1.png" alt="">
+			<img class="fig03img" src="http://sakib.localhost/wp-content/themes/twentyfifteen/images/fig03img.png" alt="">
+			<img class="fig-three-img-2" src="http://sakib.localhost/wp-content/themes/twentyfifteen/images/fig-three-img-2.png" alt="">
+			<img class="fig-three-img-3" src="http://sakib.localhost/wp-content/themes/twentyfifteen/images/fig-three-img-3.png" alt="">
+		</div>
+		<div class="nineimg"></div>
+		<div class="tenimg"></div>
+		<div class="fig-four-img">
+			<img class="fig-four-img-1" src="http://sakib.localhost/wp-content/themes/twentyfifteen/images/fig-four-img-1.png" alt="">
+			<img class="fig04img" src="http://sakib.localhost/wp-content/themes/twentyfifteen/images/fig04img.png" alt="">
+			<img class="fig-four-img-2" src="http://sakib.localhost/wp-content/themes/twentyfifteen/images/fig-four-img-2.png" alt="">
+			<img class="fig-four-img-3" src="http://sakib.localhost/wp-content/themes/twentyfifteen/images/fig-four-img-3.png" alt="">
+		</div>
+		<div class="elevenimg"></div>
+		<div class="twilveimg"></div>
+		<div class="thirtenimg"></div>
+		<div class="fortinimg"></div>
+		<div class="fig-five-img">
+			<img class="fig-five-img-1" src="http://sakib.localhost/wp-content/themes/twentyfifteen/images/fig-five-img-1.png" alt="">
+			<img class="fig05img" src="http://sakib.localhost/wp-content/themes/twentyfifteen/images/fig05img.png" alt="">
+			<img class="fig-five-img-2" src="http://sakib.localhost/wp-content/themes/twentyfifteen/images/fig-five-img-2.png" alt="">
+			<img class="fig-five-img-3" src="http://sakib.localhost/wp-content/themes/twentyfifteen/images/fig-five-img-3.png" alt="">
+		</div>
+		<div class="fiftenimg"></div>
+		<div class="footerimg">
+			<div class="treeimg"></div>
+			<div class="bird1pic"></div>
+			<div class="bird2pic"></div>
+			<div class="cho1img"></div>
+			<div class="cho2img"></div>
+		</div>
 	</section>
 	<!-- <script type="text/javascript" >
 		function clickhere(){
